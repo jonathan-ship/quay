7e862a178088185897cc45636a4765a2d143cef1
Youngin-Cho <56374406+Youngin-Cho@users.noreply.github.com>
2021-06-29 17:36:58 +0900

    simulation modeling code was added

--- environment/simulation.py
+++ environment/simulation.py
@@ -4,12 +4,13 @@ import pandas as pd
 
 
 class Work:
-    def __init__(self, name, start, finish, cut, duration_fix):
+    def __init__(self, name, start, finish, cut, duration_fix, duration):
         self.name = name
         self.start = start
         self.finish = finish
         self.cut = cut
         self.duration_fix = duration_fix
+        self.duration = duration
 
         self.working_time = self.finish - self.start + 1
         self.quay = None
@@ -25,17 +26,44 @@ class Ship:
         self.work_table = work_table
         self.fix_idx = fix_idx
 
-        self.work_list = [Work(row["작업"], row["착수(%)"], row["종료(%)"], row["자르기"], row["필수기간"])
+        self.total_duration = self.delivery_date - self.launching_date + 1
+        self.work_list = [Work(row["작업"], row["착수(%)"], row["종료(%)"], row["자르기"], row["필수기간"],
+                               self.total_duration * (int(row["종료(%)"] - int(row["착수(%)"]))) / 100)
                           for i, row in work_table.iterrows()]
         self.current_work = self.work_list[self.fix_idx]
 
 
 class Quay:
-    def __init__(self, name, length, score_table):
+    def __init__(self, env, model, name, length, score_table):
+        self.env = env
+        self.model = model
         self.name = name
         self.length = length
         self.scores = score_table.to_dict()
-        #self.priority_score = {key: value for key, value in score_table.items()}
+
+        self.queue = simpy.Store(env)
+        self.occupied = False
+        self.working_start = 0.0
+
+        self.action = self.env.process(self._run())
+
+    def _run(self):
+        while True:
+            ship = yield self.queue.get()
+            ship.current_work = ship.work_list[ship.fix_idx]
+            ship.current_work.quay = self.name
+            self.occupied = True
+
+            if ship.current_work.cut == "N":
+                working_time = ship.current_work.duration
+            else:
+                working_time = ship.current_work.duration_fix
+
+            try:
+                yield self.env.timeout(working_time)
+                ship.current_work.progress += working_time
+            except simpy.Interrupt:
+                ship.current_work.progress += (self.env.now - self.working_start)
 
 
 class Source:
@@ -46,9 +74,9 @@ class Source:
         self.model = model
         self.monitor = monitor
 
-        self.action = env.process(self.run())
+        self.action = env.process(self._run())
 
-    def run(self):
+    def _run(self):
         while True:
             ship = self.ships.pop(0)
 
