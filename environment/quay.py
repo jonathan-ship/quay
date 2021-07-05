12eb35bc50ec9b737b5f03b4b663978ffaf2ed40
Youngin-Cho <56374406+Youngin-Cho@users.noreply.github.com>
2021-07-05 19:05:38 +0900

    comments

--- environment/quay.py
+++ environment/quay.py
@@ -50,29 +50,35 @@ class QuayScheduling:
         return reward
 
     def _modeling(self):
-        sim_env = simpy.Environment()
-        monitor = Monitor(self.log_path)
+        sim_env = simpy.Environment()  # 시뮬레이션 환경 생성
+        monitor = Monitor(self.log_path)  # 시뮬레이션 이벤트 로그 기록을 위한 Monitor 객체 생성
 
+        # 안벽 배치 대상 선박에 대한 리스트 생성
         ships = []
         for i, row in self.df_ship.iterrows():
-            works = self.df_work[self.df_work["선종"] == row["선종"]]
+            works = self.df_work[self.df_work["선종"] == row["선종"]]  # 선박의 선종에 해당하는 안벽 작업 데이터프레임 선택
+            # 선박의 작업 중 안벽 고정 작업이 있는 지 확인
             if row["호선번호"] in self.df_work_fix["호선번호"]:
-                fix_name = self.df_work_fix[self.df_work_fix["호선번호"] == row["호선번호"]]["작업명"]
-                fix_idx = int(works[works["작업"] == fix_name]["순번"]) - 1
+                # 고정 작업이 있을 경우
+                fix_name = self.df_work_fix[self.df_work_fix["호선번호"] == row["호선번호"]]["작업명"]  # 고정 작업명
+                fix_idx = int(works[works["작업"] == fix_name]["순번"]) - 1  # 선박의 작업 리스트 중 고정 작업의 순서 index
             else:
+                # 고정 작업이 없을 경우
                 fix_idx = 0
 
+            # Ship 클래스에 대한 객체 생성 후 선박 리스트에 추가
             ship = Ship(row["호선번호"], row["선종"], row["길이"], row["진수일"], row["인도일"], works, fix_idx=fix_idx)
             ships.append(ship)
 
+        # 시뮬레이션 프로세스 모델링
         quays = {}
-        quays["Source"] = Source(sim_env, ships, quays, monitor)
+        quays["Source"] = Source(sim_env, ships, quays, monitor)  # Source
         for i, row in self.df_quay.iterrows():
             scores = df_score[row["안벽"]]
             quay = Quay(sim_env, row["안벽"], quays, row["길이"], scores, monitor)
-            quays[row["안벽"]] = quay
-        quays["S"] = Sea(sim_env, quays, monitor)
-        quays["Sink"] = Sink(sim_env, quays, monitor)
+            quays[row["안벽"]] = quay  # Quay
+        quays["S"] = Sea(sim_env, quays, monitor)  # Sea
+        quays["Sink"] = Sink(sim_env, quays, monitor)  # Sink
 
         return sim_env, ships, quays, monitor
 
